ab135e8e2605c88613eb62c541c8b33080448f84
Andrew Wittrock <andy@wittrock.us>
2017-04-12 23:04:59 -0500

    Refactor out monkeypatching

--- src/reducer/entities.js
+++ src/reducer/entities.js
@@ -3,17 +3,18 @@
  */
 
 import {mergeFlags} from "atp-sugar";
+import {o} from "atp-sugar";
 
 export const UPDATE_ENTITY = "atp-entity/update";
 export const UPDATE_ENTITIES = "atp-entity/update-multiple";
 
-const _updateEntity = (state, type, entity) => state.$merge({
+const _updateEntity = (state, type, entity) => o(state).merge({
     [type]: {
-        [entity.id]: (
+        [entity.id]: o(
             state[type] && state[type][entity.id] ? state[type][entity.id] : {}
-        ).$merge(entity, mergeFlags.NONE)
+        ).merge(entity, mergeFlags.NONE).raw
     }
-}, mergeFlags.RECURSIVE);
+}, mergeFlags.RECURSIVE).raw;
 
 const _updateEntities = (state, type, entities) => entities.reduce(
     (combined, entity) => _updateEntity(combined, type, entity),
@@ -21,7 +22,7 @@ const _updateEntities = (state, type, entities) => entities.reduce(
 );
 
 export default (state = {}, action) =>
-    action.type.$switch({
+    o(action.type).switch({
         [UPDATE_ENTITY]:   () => _updateEntity(state, action.entityType, action.entity),
         [UPDATE_ENTITIES]: () => _updateEntities(state, action.entityType, action.entities),
         default: () => state
